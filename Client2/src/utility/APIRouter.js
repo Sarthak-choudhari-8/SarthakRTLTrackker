480a66e62524435da7952cecb6b84f05b9bbf57f
Your Name <your.email@example.com>
2025-05-21 14:51:34 +0530

    third

--- Client2/src/utility/APIRouter.js
+++ Client2/src/utility/APIRouter.js
@@ -1,18 +1,18 @@
-export const host = "https://sarthak-rtl-trackker-backend.vercel.app/";
+export const host = "http://localhost:8080/";
 
 
- export const addTodoRoute = `${host}postTodo`;
-export const getTodosRoute = `${host}getTodos`
-export const deleteTodoRoute = `${host}deleteTodo`
-export const markDoneTodoRoute = `${host}markDoneTodo`
+ export const addTodoRoute = `${host}/postTodo`;
+export const getTodosRoute = `${host}/getTodos`
+export const deleteTodoRoute = `${host}/deleteTodo`
+export const markDoneTodoRoute = `${host}/markDoneTodo`
 
-export const getBalanceRoute = `${host}getBalance`
-export const setBalanceRoute = `${host}setBalance`
-export const setFinanceListRoute = `${host}setFinanceList`
-export const getFinanceListRoute = `${host}getFinanceList`
-export const deleteFinanceListRoute = `${host}deleteFinanceList`
+export const getBalanceRoute = `${host}/getBalance`
+export const setBalanceRoute = `${host}/setBalance`
+export const setFinanceListRoute = `${host}/setFinanceList`
+export const getFinanceListRoute = `${host}/getFinanceList`
+export const deleteFinanceListRoute = `${host}/deleteFinanceList`
 
-export const getSecureListRoute = `${host}getSecureList`
-export const setSecureListRoute = `${host}setSecureList`
-export const deleteSecureListRoute = `${host}deleteSecureList`
-export const getPasswordRoute = `${host}getPassword`
+export const getSecureListRoute = `${host}/getSecureList`
+export const setSecureListRoute = `${host}/setSecureList`
+export const deleteSecureListRoute = `${host}/deleteSecureList`
+export const getPasswordRoute = `${host}/getPassword`
